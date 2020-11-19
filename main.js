7977a979ee26ce7de6945862b72f567f4e3197f5
amd597 <amd597@icloud.com>
2020-11-18 20:46:16 -0500

    final

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 $(function(){
-    var $newItemButton, $newItemForm;
+    var $newItemButton, $newItemForm, $list;
     $newItemButton = $('#newItemButton');
     $newItemForm = $('#newItemForm');
+    $list = $('ul');
+
 
     $newItemButton.show();
     $newItemForm.hide();
@@ -10,4 +12,21 @@ $(function(){
         $newItemForm.show();
     })
 
+    $newItemForm.on('submit', function(e){
+        e.preventDefault();
+        var text = $('input:text').val();
+        $list.append('<li>' + text + '</li>');
+        $('input:text').val('');   
+    })
+
+    $list.on('click', 'li', function() {
+        var $this = $(this);              
+        $this.animate({                  
+            opacity: 0.0,
+            paddingLeft: '+=180'
+          }, 500, 'swing', function() {    
+            $this.remove();                
+          });
+             
+        })        
 })
